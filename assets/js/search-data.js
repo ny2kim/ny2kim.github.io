b741c989b601637e5053bb97b1ad4969d1fa0c67
ny2kim <108639420+ny2kim@users.noreply.github.com>
2025-09-07 07:23:27 +0000

    Deploying to gh-pages from @ ny2kim/ny2kim.github.io@7b42ac0738d69fca0a3d46a59aca22705b9397bc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "",
           section: "Navigation",
           handler: () => {
